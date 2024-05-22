bd6f88700955c9f18db91488da8989dab2774e03
WillWSmith <will.w.smith94@gmail.com>
2024-05-22 09:07:02 -0400

    Add About Us page with content/link to it

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'about/index'
   root to: 'products#index'
 
   resources :products, only: [:index, :show]
@@ -16,6 +17,8 @@ Rails.application.routes.draw do
     resources :products, except: [:edit, :update, :show]
   end
 
+  get 'about', to: 'about#index', as: 'about'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
