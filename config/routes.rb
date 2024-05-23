f2809a80a136c0b2a1a013b38e49a25663be9d9d
WillWSmith <will.w.smith94@gmail.com>
2024-05-23 08:53:03 -0400

    Add users table, account creation/login/auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,28 +1,25 @@
 Rails.application.routes.draw do
-  namespace :admin do
-    get 'categories/index'
-    get 'categories/new'
-    get 'categories/create'
-  end
-  get 'about/index'
   root to: 'products#index'
 
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
 
   resource :cart, only: [:show] do
-    post   :add_item
-    post   :remove_item
+    post :add_item
+    post :remove_item
   end
 
   resources :orders, only: [:create, :show]
 
+  get 'about', to: 'about#index', as: 'about'
+  
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
     resources :categories, only: [:index, :new, :create]
   end
 
-  get 'about', to: 'about#index', as: 'about'
-  
+  resources :users, only: [:new, :create]
+  resource :sessions, only: [:new, :create, :destroy]
 end
+
